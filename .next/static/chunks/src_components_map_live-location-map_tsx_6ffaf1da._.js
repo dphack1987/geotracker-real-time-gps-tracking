451e6d122a2e3f54cf1d969986df524a25fdcc18
dphack <proyectoreact894@gmail.com>
2025-05-26 15:45:50 -0500

    Add .next/static/chunks/src_components_map_live-location-map_tsx_6ffaf1da._.js 1748292350517

---
+++ .next/static/chunks/src_components_map_live-location-map_tsx_6ffaf1da._.js
@@ -0,0 +1,17 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([typeof document === "object" ? document.currentScript : undefined, {
+
+"[project]/src/components/map/live-location-map.tsx [app-client] (ecmascript, next/dynamic entry, async loader)": ((__turbopack_context__) => {
+
+var { g: global, __dirname } = __turbopack_context__;
+{
+__turbopack_context__.v((parentImport) => {
+    return Promise.all([
+  "static/chunks/src_components_map_map-component_tsx_90738462._.js",
+  "static/chunks/src_components_map_6f07aebe._.js",
+  "static/chunks/src_components_map_live-location-map_tsx_4bfad78c._.js"
+].map((chunk) => __turbopack_context__.l(chunk))).then(() => {
+        return parentImport("[project]/src/components/map/live-location-map.tsx [app-client] (ecmascript, next/dynamic entry)");
+    });
+});
+}}),
+}]);
