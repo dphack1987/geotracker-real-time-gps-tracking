53c70283d35f9c7f9e74995e87700501faefd0c1
dphack <proyectoreact894@gmail.com>
2025-06-07 15:22:44 -0500

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1749327764135

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_components_map_map-component_tsx_a9bb4ab3._.js",
+  "static/chunks/src_d0415745._.js",
+  "static/chunks/node_modules_9ccb3f39._.js"
+],
+    source: "dynamic"
+});
